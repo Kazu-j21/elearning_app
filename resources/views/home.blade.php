66f1177637107cc8da8941079518c3b7f621a0c9
Kazu-j21 <nishiadgjmptw1021@gmail.com>
2020-03-01 19:58:16 +0000

    Login , Register, Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,30 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+
+        <div class="col-md-4">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <img src="image/user-image.png" class="card-img-top" alt="">
+                <h5 class="card-title">{{Auth::user()->name}}</h5>
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+
+            <div class="card">
+
+            </div>
+        </div>
+
+
+        <div class="col-md-8">
+            <div class="card mb-4">
+                <div class="card-header">ACTIVITY FEED</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,11 +34,12 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <p> Nothing to show</p> 
                 </div>
             </div>
         </div>
+
+        
     </div>
 </div>
 @endsection
