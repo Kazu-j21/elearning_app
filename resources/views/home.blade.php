8782a02bb0e38fcbdec42809cbeccff187d0d098
Kazu-j21 <nishiadgjmptw1021@gmail.com>
2020-03-11 17:28:31 +0900

    fixed format

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,31 +3,24 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-
         <div class="col-md-4">
             <div class="card">
                 <img src="image/user-image.png" class="card-img-top" alt="">
-                <h5 class="card-title">{{Auth::user()->name}}</h5>
+                <h5 class="card-title">{{ Auth::user()->name }}</h5>
             </div>
-
             <div class="card">
 
             </div>
-
             <div class="card">
 
             </div>
-
             <div class="card">
 
             </div>
         </div>
-
-
         <div class="col-md-8">
             <div class="card mb-4">
                 <div class="card-header">ACTIVITY FEED</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
@@ -38,8 +31,6 @@
                 </div>
             </div>
         </div>
-
-        
     </div>
 </div>
 @endsection
